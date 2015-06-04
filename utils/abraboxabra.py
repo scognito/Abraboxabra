41aad2dd925d0096df5897ddba035a3dac8a0a0e
unknown <scognito@gmail.com>
2015-06-04 21:39:32 +0200

    Added multithreading support

--- utils/abraboxabra.py
+++ utils/abraboxabra.py
@@ -3,8 +3,8 @@
 
 import time
 import RPi.GPIO as GPIO
-import os
 from bluetooth import *
+from thread import *
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -16,55 +16,64 @@ first_time = True
 
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
+server_sock.listen(10)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
+advertise_service( server_sock, "Abraboxabra server",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],
-#                   protocols = [ OBEX_UUID ]
-                  )
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+
+#GPIO.output(7,True)
 
 print "Waiting for connection on RFCOMM channel %d" % port
 
-client_sock, client_info = server_sock.accept()
-print "Accepted Bluetooth connection from ", client_info
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+     
+    global first_time
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+        try: 
+		#Receiving from client
+		data = conn.recv(1024)
+		print "received command [%s]" % data
 
+		client_sock.send('received message ' + data);
+		if data == 'openclose':
+			if first_time:
+				GPIO.setup(7, GPIO.OUT)
+				first_time = False
+
+			GPIO.output(7, False)
+			time.sleep(2)
+			GPIO.output(7,True)
+			client_sock.send("OK")
+	except IOError:
+		print "Disconnected from ", client_info
+		pass
+     
+    #came out of loop
+    conn.close()
+
+#
+# MAIN
+#
 while True:
-   try:
-        data = client_sock.recv(1024)
-#       if len(data) == 0: break
-        print "received command [%s]" % data
-#       client_sock.send('received message: ' + data);
-        if data == 'openclose':
-                if first_time:
-                        GPIO.setup(7, GPIO.OUT)
-                        first_time = False
+	client_sock, client_info = server_sock.accept()
+	print "Accepted Bluetooth connection from ", client_info
 
-                # simulate pressing button for 1 second
-                GPIO.output(7,False)
-                time.sleep(1)
-                GPIO.output(7,True)
-                client_sock.send("OK")
-        elif data == 'shutdown':
-                os.system("sudo shutdown -h now")
-                client_sock.send("OK")
-
-   except IOError:
-      client_sock, client_info = server_sock.accept()
-      print "Accepted Bluetooth connection from ", client_info
-      pass
+	start_new_thread(clientthread ,(client_sock,))
 
 print "disconnected"
 
 time.sleep(1)
-print "DEBUG: fine, 7 true"
 GPIO.output(7,True)
 
-client_sock.close()
 server_sock.close()
-print "All done, have a nice day :)"
+print "all done"
